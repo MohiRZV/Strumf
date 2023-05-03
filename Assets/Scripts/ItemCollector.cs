0244ab096b6fb18cb0ca3fbbe06773af4c9463f5
MohiRZV <rsalsigan@gmail.com>
2023-05-03 23:29:38 +0300

    generic item collector, moved logic to game controller, added apple task

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,48 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
-    private string TAG = "ItemCollector";
-    private int cherries = 0;
-    private int heldCherries = 0;
-
-    [SerializeField] private Text cherriesText;
 
     public GameController gameControllerObj;
+    [SerializeField] private GameController.Collectible collectible;
+    [SerializeField] private GameController.Collectible wrongCollectible = GameController.Collectible.None;
     private void OnTriggerEnter2D(Collider2D collision) {
-        
-        // lazy instantiate the game controller
+         // lazy instantiate the game controller
         if (gameControllerObj == null) {
             gameControllerObj = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
         }
-
         Debug.Log("Collision "+collision.tag);
-        if (collision.gameObject.CompareTag("Cherry")) {
-            // can't carry more than one cherry at a time
-            if (heldCherries<1) {
-                // destroy the game object the player collided with
-                Destroy(collision.gameObject);
-                heldCherries++;
-                gameControllerObj.collected(GameController.Collectible.CHERRY);
-            } else {
-                Debug.Log(TAG + "# can't pick up more than one cherry at a time");
-            }
+        // Send the collision object to the GameController if it is one of the desired collisions
+        if (collision.tag == collectible.ToString()) {
+            gameControllerObj.collected(collectible, collision.gameObject);
         }
-
-
-        if (collision.gameObject.CompareTag("Box")) {
-            // deposit the cherry
-            if (heldCherries > 0) {
-                cherries++;
-                // display the number of collected cherries
-                cherriesText.text = "Cirese: " + cherries;
-                heldCherries--;
-            } else {
-                Debug.Log(TAG + "# nothing to deposit");
-            }
+        if (collision.tag == wrongCollectible.ToString()) {
+            gameControllerObj.collected(wrongCollectible, collision.gameObject);
         }
     }
 }
